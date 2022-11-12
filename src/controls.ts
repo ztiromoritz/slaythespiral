b770c9f0a6de7ae5d736d08f891870d611425961
Moritz Rebbert <moritz.rebbert@udo.edu>
2022-11-12 14:18:01 +0100

    keyboard controls

---
+++ src/controls.ts
@@ -0,0 +1,44 @@
+export function initControls() {
+  const KEYS = {
+    up: false,
+    down: false,
+    left: false,
+    right: false,
+  };
+
+  document.addEventListener("keydown", (event) => {
+    switch (event.key) {
+      case "ArrowLeft":
+        KEYS.left = true;
+        break;
+      case "ArrowRight":
+        KEYS.right = true;
+        break;
+      case "ArrowUp":
+        KEYS.up = true;
+        break;
+      case "ArrowDown":
+        KEYS.down = true;
+        break;
+    }
+  });
+
+  document.addEventListener("keyup", (event) => {
+    switch (event.key) {
+      case "ArrowLeft":
+        KEYS.left = false;
+        break;
+      case "ArrowRight":
+        KEYS.right = false;
+        break;
+      case "ArrowUp":
+        KEYS.up = false;
+        break;
+      case "ArrowDown":
+        KEYS.down = false;
+        break;
+    }
+  });
+
+  return { KEYS };
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import { setupCounter } from "./counter";
 
-import * as PIXI from "pixi.js";
+import { Application, Sprite, Texture } from "pixi.js";
 
-const app = new PIXI.Application({ width: 640, height: 360 });
+import { initControls } from "./controls";
+const { KEYS } = initControls();
+
+const app = new Application({ width: 640, height: 360 });
 document.body.appendChild(app.view);
+
+const sprite = Sprite.from("public/char.png");
+
+sprite.anchor.set(0.5);
+sprite.x = 40;
+sprite.y = 40;
+app.stage.addChild(sprite);
+
+let x;
+
+///
+app.ticker.add((delta) => {
+  if (KEYS.up) {
+    sprite.x += delta * 4;
+  }
+});
+
+console.log("hello worlddd");
